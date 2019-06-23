4f31edfa1ea5f93283942dd4154b4d63e06d0c6a
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 10:19:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -61,7 +61,7 @@ def merge_data (key, data)
     puts ele
       ele.each do |info, value|
         if nameKey == info 
-          finalHas = {item.key(nameKey) => nameKey}
+          finalHas = {item.keys(nameKey) => nameKey}
           final << finalHas.merge(value)
       end
       end
